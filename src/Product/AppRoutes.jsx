2a0ac712a5a4eea9f3dab0d00291e8bd61dab0d7
Gnanaparthiban M <gnanaparthiban9655@gmail.com>
2026-02-16 16:33:42 +0530

    bulk

--- src/Product/AppRoutes.jsx
+++ src/Product/AppRoutes.jsx
@@ -1,17 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import ProductList from "./pages/ProductList";
 import ProductForm from "./pages/ProductForm";
+import ProductBulkUpload from "./pages/ProductBulkUpload";
 import CategoryList from "./pages/CategoryList";
 import CategoryAdd from "./pages/CategoryAdd";
 import SubcategoryList from "./pages/SubcategoryList";
 import SubcategoryForm from "./pages/SubcategoryForm";
-import { List, Tags, Layers } from "lucide-react";
+import { List, Tags, Layers, Upload } from "lucide-react";
 
 export const ProductMenuItems = [
   {
     key: "/Product/list",
     label: "Products",
     icon: <List size={18} />,
+  },
+  {
+    key: "/Product/bulk-upload",
+    label: "Bulk Upload",
+    icon: <Upload size={18} />,
   },
   {
     key: "/Product/categories",
@@ -31,6 +37,7 @@ const ProductRoutes = () => {
       <Route path="list" element={<ProductList />} />
       <Route path="add" element={<ProductForm />} />
       <Route path="edit/:id" element={<ProductForm />} />
+      <Route path="bulk-upload" element={<ProductBulkUpload />} />
 
       <Route path="categories" element={<CategoryList />} />
       <Route path="categories/add" element={<CategoryAdd />} />
